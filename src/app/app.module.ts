fd14e8e9037549f14ad29fdf74f6eac920ea7a41
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-09-20 16:01:10 +0044

    n g c novo componente - servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent
+    AppComponent, ServerComponent, ServersComponent
   ],
   imports: [
     BrowserModule, FormsModule 
