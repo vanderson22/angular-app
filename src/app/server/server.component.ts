7c22116f99fa644805a4a700b68f49a2c6e5d7ed
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-09-20 15:25:10 +0044

    Criando componente de testes

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//descorator para informar que é um component
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+ 
+  })
+  
+export class ServerComponent{
+    
+}
