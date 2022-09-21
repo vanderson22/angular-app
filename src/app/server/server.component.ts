ebab8e44cc4a3aa02dc208dea434939320fa1895
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-09-21 09:38:53 +0044

    String interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,5 +8,10 @@ import { Component } from "@angular/core";
   })
   
 export class ServerComponent{
-    
+    serverId = 10;
+    serverStatus = 'offline'
+
+    getServerStatus(){
+      return this.serverStatus;
+    }
 }
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -11,5 +11,6 @@ export class WarningAlertComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
 
 }
